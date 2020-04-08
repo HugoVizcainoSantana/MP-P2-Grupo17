13f2c349ee2a188a45526d3a025b4ae0d74ec16e
Sergio <>
2020-04-08 20:03:08 +0200

    Comentario class

--- src/posts/Comentario.java
+++ src/posts/Comentario.java
@@ -1,14 +1,21 @@
 package posts;
 
+import users.Usuario;
+
 class Comentario{
     
 private String texto;
 private int puntuacion;
 
     public void comentar (String texto){
+        this.texto = texto;
     }
-    public boolean votar (int valor){
-        return true;
+    public boolean votar (int valor, Usuario usuarioComent, Usuario usuarioVoto){
+        if(usuarioVoto != usuarioComent){
+            puntuacion = puntuacion + valor;
+            return true;
+        }else
+            return false;
     }
 
 
