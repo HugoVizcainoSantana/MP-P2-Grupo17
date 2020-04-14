d24acb471bf9d3204bfaae8cf711fd4d4e5c3bf1
Hugo Samuel Vizcaino Santana <hugo.vizcaino.santana@gmail.com>
2020-04-14 17:58:38 +0200

    Notify users suscribed to subforum

--- src/mp/g17/Subforo.java
+++ src/mp/g17/Subforo.java
@@ -1,6 +1,8 @@
 package mp.g17;
 
 import java.util.List;
+
+import mp.g17.events.EventoEntradaCreada;
 import mp.g17.users.Usuario;
 import mp.g17.posts.EntradaGenerica;
 
@@ -8,7 +10,7 @@ public class Subforo {
     private String name;
     private List<Usuario> usersForo;
     private List<EntradaGenerica> inputSubforo;
-    
+
     public String getName() {
         return name;
     }
@@ -16,25 +18,25 @@ public class Subforo {
     public void setName(String nombre) {
         this.name = nombre;
     }
-    
+
     public void addUser (Usuario subs){
         usersForo.add(subs);
     }
-    
+
     public void addInput (EntradaGenerica input){
         inputSubforo.add(input);
     }
-    
+
     public void deleteUser (Usuario subs){
         usersForo.remove(subs);
     }
-    
+
     public void deleteInput (EntradaGenerica input){
         inputSubforo.remove(input);
     }
-    
-    public void notify (){
-        getNotifications().add("Nueva alta en el foro:" + name);
+
+    private void notifyUsers (){
+        usersForo.forEach(user -> user.update(new EventoEntradaCreada(this,null)));
     }
 
 
