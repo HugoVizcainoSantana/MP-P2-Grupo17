0573e73f6d85b4e575466d1780744bf8e3e74679
Hugo Samuel Vizcaino Santana <hugo.vizcaino.santana@gmail.com>
2020-04-18 09:46:50 +0200

    Small refactor demostrator

--- src/mp/g17/Sistema.java
+++ src/mp/g17/Sistema.java
@@ -1,103 +1,116 @@
 package mp.g17;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import mp.g17.users.Alumno;
+import mp.g17.users.Profesor;
 import mp.g17.users.Usuario;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
-import mp.g17.users.Profesor;
 
-public class Sistema {
+public class Sistema implements Serializable {
 
+    // Constantes para facilitar el uso del demostrador/sistema
+    public static final String CORREO_HUGO = "hs.vizcaino@alumnos.urjc.es";
+
+    // Variables de sistema
     private static Map<String, Usuario> users = new HashMap<>();
     private static Usuario currentUser = null;
-    private static List<Usuario> registeredUsers = new ArrayList<>();
     private static List<Subforo> subforums = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Inicializando sistema!");
-        Usuario alumno1 = new Alumno("Hugo", "Vizcaino", "hvizcaino", "hvizcaino@alumnos.urjc.es", "12345");
-        System.out.println("Creado alumno1 = " + alumno1);
-        users.put(alumno1.getEmail(), alumno1);
+        if (registerUser("Hugo", "Vizcaino", "hugo1234", CORREO_HUGO, "hVizcaino")) {
+            System.out.println("Usuario registrado correctamente");
+        }
+        if (login(CORREO_HUGO, "hugo1235")) {
+            System.out.println("Se ha iniciado sesion con las credenciales " + CORREO_HUGO + ":hugo1235");
+        } else {
+            System.out.println("No se ha podido iniciar sesion con las credenciales " + CORREO_HUGO + ":hugo1235");
+        }
+        if (login(CORREO_HUGO, "hugo1234")) {
+            System.out.println("Se ha iniciado sesion con las credenciales " + CORREO_HUGO + ":hugo1234");
+        } else {
+            System.out.println("No se ha podido iniciar sesion con las credenciales " + CORREO_HUGO + ":hugo1235");
+        }
+
+        System.out.println("El usuario actual (con sesion iniciada) es:\n" + currentUser);
     }
 
 
     public static boolean login(String email, String password) {
+        String auth = email + ":" + password;
+        System.out.println("Intento de inicio de sesion con credenciales " + auth);
         Usuario userByEmail = users.get(email);
-        if (userByEmail != null && userByEmail.getPassword().equals(password)) {
-            currentUser = userByEmail;
-            return true;
+        if (userByEmail != null) {
+            if (userByEmail.getPassword().equals(password)) {
+                currentUser = userByEmail;
+                System.out.println("Login correcto! Credenciales: " + auth);
+                return true;
+            }
+            System.err.println("El correo recibido no existe");
         }
+
         return false;
     }
 
     public static boolean logout() {
+        System.out.println("Haciendo logout...");
         currentUser = null;
         return true;
     }
-    
-    public static boolean login(){
-        return true;
+
+    public static boolean registerUser(String name, String surname, String password, String mail, String alias) {
+        Usuario user;
+        if (users.containsKey(mail)) {
+            System.err.println("Ya existe un usuario con el correo " + mail);
+        }
+        if (mail.endsWith("@alumnos.urjc.es")) {
+            user = new Alumno(name, surname, alias, mail, password);
+            System.out.println("Registrando nuevo alumno con correo " + mail);
+            users.put(mail, user);
+            return true;
+        } else if (mail.endsWith("@urjc.es")) {
+            user = new Profesor(name, surname, alias, mail, password);
+            System.out.println("Registrando nuevo profesor con correo " + mail);
+            users.put(mail, user);
+            return true;
+        } else {
+            System.out.println("No es un email valido");
+            return false;
+        }
+
     }
-    public static boolean registerUser(String name, String surname, String password, String mail, String alias){
-        Usuario user = null;
-            if(mail.contains("@alumnos.urjc.es")){
-                user = new Alumno(name, surname, alias, mail, password);
-                registeredUsers.add(user);
-                return true;
-            }else if(mail.contains("@urjc.es")){
-                user = new Profesor(name, surname, alias, mail, password);
-                registeredUsers.add(user);
-                return true;
-            }else{
-                System.out.println("No es un email valido");
-                return false;
-            }
-            
-            
-    }
-    
-    public static boolean createSubforum(String name){
-        Subforo forum = null;
-        forum.setName(name);
+
+    public static boolean createSubforum(String name) {
+        Subforo forum = new Subforo(name);
         subforums.add(forum);
         return true;
     }
-    
-    private static boolean save(Sistema system){
-        try {
-           FileOutputStream file = new FileOutputStream("forum.obj");
-           ObjectOutputStream finalFile = new ObjectOutputStream(file);
-           finalFile.writeObject(system);
-           finalFile.close();
-           file.close();
-           return true;
 
+    private static boolean save(Sistema system) {
+        try (FileOutputStream file = new FileOutputStream("forum.obj")) {
+            try (ObjectOutputStream output = new ObjectOutputStream(file)) {
+                output.writeObject(system);
+                return true;
+            }
         } catch (IOException e) {
-           System.out.println(e.getMessage());
-           return false;
+            System.err.println(e.getMessage());
+            return false;
         }
     }
-    
-    public static Sistema load(){
+
+    public static Sistema load() {
         Sistema system = null;
-        try {
-            FileInputStream file = new FileInputStream("forum.obj");
-            ObjectInputStream inputFile = new ObjectInputStream(file);
-            system = (Sistema) inputFile.readObject();
-            
-            inputFile.close();
-            file.close();
+        try (FileInputStream file = new FileInputStream("forum.obj")) {
+            try (ObjectInputStream input = new ObjectInputStream(file)) {
+                system = (Sistema) input.readObject();
+            }
         } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.exit(-1);
+            System.err.println(e.getMessage());
+            System.exit(-1);
         }
         return system;
     }
