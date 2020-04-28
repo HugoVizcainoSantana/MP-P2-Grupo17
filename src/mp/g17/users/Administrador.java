881a20d06440d500e508f908dfb054119b6f9f2c
sarry150 <sergiosarriaporro@gmail.com>
2020-04-28 17:35:58 +0200

    Update Administrador.java

--- src/mp/g17/users/Administrador.java
+++ src/mp/g17/users/Administrador.java
@@ -4,26 +4,26 @@ import mp.g17.Penalizacion;
 import mp.g17.Subforo;
 import mp.g17.posts.EntradaGenerica;
 
-import java.util.Calendar;
+
 import java.util.GregorianCalendar;
 
-public class Administrador extends Usuario {
-    public Administrador(String email, String password) {
+public class Administrador extends Usuario { 
+    public Administrador(String email, String password) { //Constructor of the administrator user type
         super("Administrador", "", "Administrador", email, password);
     }
 
-    public void penalizarUsuario(Alumno alumno, String reason) {
+    public void penalizarUsuario(Alumno alumno, String reason) { //This method penalizes a member from the forum
         alumno.setStrike(new Penalizacion(new GregorianCalendar(), this, reason));
     }
-    public void verify(EntradaGenerica entrada,boolean resultado){
+    public void verify(EntradaGenerica entrada,boolean resultado){ //If a post does not get verified the author becomes banned
         entrada.verify(resultado);
         if(resultado==false){
             if(entrada.getCreatedBy() instanceof Alumno){
                 penalizarUsuario((Alumno) entrada.getCreatedBy(),"No cumple con los requisitos para ser publicada");
-            };
+            }
         }
     }
-    public void updatePosts(Subforo subforo){
+    public void updatePosts(Subforo subforo){ //This method refresh which post are available to be seen
         subforo.setVerifiedInVisiblePosts();
 
     }
