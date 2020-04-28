1abe3287fa481a318d13ec5068dc453fb73caf54
sarry150 <sergiosarriaporro@gmail.com>
2020-04-28 17:44:37 +0200

    Update IVotable.java

--- src/mp/g17/posts/IVotable.java
+++ src/mp/g17/posts/IVotable.java
@@ -22,16 +22,15 @@ public interface IVotable {
 
     default void vote(Boolean valor, Usuario userVoting) {
         Map<Usuario, Boolean> usersVotes = getUsersVotes();
-        if (usersVotes.containsKey(userVoting)) {
-            boolean votoAnterior = usersVotes.remove(userVoting);
-            if (votoAnterior) {
+        if (usersVotes.containsKey(userVoting)) { //This check if the user that is voting has voted before
+            boolean votoAnterior = usersVotes.remove(userVoting); //If is the case the vote is removed (it doesn't count)
+            if (votoAnterior) { 
                 decrementVotes();
             } else {
                 incrementVotes();
             }
         }
-        // If the new vote its null, we don't add the vote back, it simply gets removed
-        if (valor != null) {
+        if (valor != null) {  // If the new vote is null, we don't add the vote back, it simply gets removed
             if (valor) {
                 incrementVotes();
             } else {
