7aea66bb2da3d76cff22028d094299d50fbee283
sarry150 <sergiosarriaporro@gmail.com>
2020-04-28 17:36:30 +0200

    Update Alumno.java

--- src/mp/g17/users/Alumno.java
+++ src/mp/g17/users/Alumno.java
@@ -6,12 +6,12 @@ import mp.g17.events.EventoEntradaCreada;
 public class Alumno extends Usuario {
     private Penalizacion strike;
 
-    public Alumno(String firstname, String lastname, String alias, String email, String password) {
+    public Alumno(String firstname, String lastname, String alias, String email, String password) { //Constructor of the alumn user type
         super(firstname, lastname, alias, email, password);
     }
 
     @Override
-    public void update(EventoEntradaCreada event) {
+    public void update(EventoEntradaCreada event) { //This method returns the alert of have created a new post
         System.out.println("Soy el Alumno " + email + " que ha recibido una notificacion de post creado");
         super.update(event);
     }
@@ -20,7 +20,7 @@ public class Alumno extends Usuario {
         return strike;
     }
 
-    public void setStrike(Penalizacion strike) {
+    public void setStrike(Penalizacion strike) { //This method gives the strike to the alumn
         System.out.println("El alumno " + email + " esta recibiendo una penalizacion de " + strike.getPenalizedBy().getEmail() + ".\n\tMotivo: " + strike.getReason());
         this.strike = strike;
     }
