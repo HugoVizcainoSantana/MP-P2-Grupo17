1c12793dbf83ae0df09bd44cd4e9afdfe844c959
Hugo Samuel Vizcaino Santana <hugo.vizcaino.santana@gmail.com>
2020-05-14 17:51:05 +0200

    Tests de Encuesta y PreguntaEncuesta

--- test/mp/g17/posts/EncuestaTest.java
+++ test/mp/g17/posts/EncuestaTest.java
@@ -5,15 +5,31 @@
  */
 package mp.g17.posts;
 
+import mp.g17.users.Alumno;
+import mp.g17.users.Profesor;
+import mp.g17.users.Usuario;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 /**
- *
  * @author usuario
  */
 public class EncuestaTest {
 
+    private final Profesor profesor;
+    private final Usuario userTest1;
+    private final Usuario userTest2;
+    private final Usuario userTest3;
+
     public EncuestaTest() {
+        profesor = new Profesor("profe1", "ape1", "profeMolon", "profemolon@urjc.es", "asereje");
+        userTest1 = new Alumno("Hugo", "Vizcaino", "hvizcaino", "hugo@urjc.es", "1234");
+        userTest2 = new Alumno("Hugo2", "Vizcaino2", "hvizcaino2", "hugo2@urjc.es", "1234");
+        userTest3 = new Alumno("Hugo3", "Vizcaino3", "hvizcaino3", "hugo3@urjc.es", "1234");
     }
 
     /**
@@ -21,6 +37,9 @@ public class EncuestaTest {
      */
     @Test
     public void testAddQuestion() {
+        Encuesta encuesta = new Encuesta(profesor, "Encuesta de satisfaccion");
+        encuesta.addQuestion(new PreguntaEncuesta("¿Estais de acuerdo con el contenido que se enseña?"));
+        assertEquals(1, encuesta.getPolls().size());
     }
 
     /**
@@ -28,20 +47,11 @@ public class EncuestaTest {
      */
     @Test
     public void testDeleteQuestion() {
-    }
-
-    /**
-     * Test of getPolls method, of class Encuesta.
-     */
-    @Test
-    public void testGetPolls() {
-    }
-
-    /**
-     * Test of setPolls method, of class Encuesta.
-     */
-    @Test
-    public void testSetPolls() {
+        Encuesta encuesta = new Encuesta(profesor, "Encuesta de satisfaccion");
+        PreguntaEncuesta preguntaEncuesta = new PreguntaEncuesta("¿Estais de acuerdo con el contenido que se enseña?");
+        encuesta.addQuestion(preguntaEncuesta);
+        encuesta.deleteQuestion(preguntaEncuesta);
+        assertEquals(0, encuesta.getPolls().size());
     }
 
     /**
@@ -49,6 +59,27 @@ public class EncuestaTest {
      */
     @Test
     public void testGetAllAnswersAnonymously() {
+        Encuesta encuesta = new Encuesta(profesor, "Encuesta de satisfaccion");
+        PreguntaEncuesta preguntaEncuesta1 = new PreguntaEncuesta("¿Estas de acuerdo con el contenido que se enseña?");
+        preguntaEncuesta1.addAnswer("Si");
+        preguntaEncuesta1.addAnswer("No");
+        encuesta.addQuestion(preguntaEncuesta1);
+        PreguntaEncuesta preguntaEncuesta2 = new PreguntaEncuesta("¿Estas contento con el formato de la asignatura?");
+        preguntaEncuesta2.addAnswer("Si");
+        preguntaEncuesta2.addAnswer("No");
+        encuesta.addQuestion(preguntaEncuesta2);
+        encuesta.getPolls().forEach(preguntaEncuesta -> {
+            preguntaEncuesta.answer(userTest1, "Si");
+            preguntaEncuesta.answer(userTest2, "No");
+            preguntaEncuesta.answer(userTest3, "Niet");
+        });
+        Map<String, Map<String, Long>> respuestasEsperadas = new HashMap<>();
+        Map<String, Long> resultadosPregunta = new HashMap<>();
+        resultadosPregunta.put("Si", 1L);
+        resultadosPregunta.put("No", 1L);
+        respuestasEsperadas.put("¿Estas de acuerdo con el contenido que se enseña?", resultadosPregunta);
+        respuestasEsperadas.put("¿Estas contento con el formato de la asignatura?", resultadosPregunta);
+        assertEquals(respuestasEsperadas, encuesta.getAllAnswersAnonymously());
     }
 
     /**
@@ -56,6 +87,28 @@ public class EncuestaTest {
      */
     @Test
     public void testGetAllAnswers() {
+        Encuesta encuesta = new Encuesta(profesor, "Encuesta de satisfaccion");
+        PreguntaEncuesta preguntaEncuesta1 = new PreguntaEncuesta("¿Estas de acuerdo con el contenido que se enseña?");
+        preguntaEncuesta1.addAnswer("Si");
+        preguntaEncuesta1.addAnswer("No");
+        encuesta.addQuestion(preguntaEncuesta1);
+        PreguntaEncuesta preguntaEncuesta2 = new PreguntaEncuesta("¿Estas contento con el formato de la asignatura?");
+        preguntaEncuesta2.addAnswer("Si");
+        preguntaEncuesta2.addAnswer("No");
+        encuesta.addQuestion(preguntaEncuesta2);
+        encuesta.getPolls().forEach(preguntaEncuesta -> {
+            preguntaEncuesta.answer(userTest1, "Si");
+            preguntaEncuesta.answer(userTest2, "Niet");
+            preguntaEncuesta.answer(userTest3, "No");
+        });
+        Map<String, Map<Usuario, String>> respuestasEsperadas = new HashMap<>();
+        Map<Usuario, String> resultadosPregunta = new HashMap<>();
+        resultadosPregunta.put(userTest1, "Si");
+        resultadosPregunta.put(userTest3, "No");
+        respuestasEsperadas.put("¿Estas de acuerdo con el contenido que se enseña?", resultadosPregunta);
+        respuestasEsperadas.put("¿Estas contento con el formato de la asignatura?", resultadosPregunta);
+        assertEquals(respuestasEsperadas, encuesta.getAllAnswers());
+
     }
 
 }
--- test/mp/g17/posts/PreguntaEncuestaTest.java
+++ test/mp/g17/posts/PreguntaEncuestaTest.java
@@ -5,15 +5,28 @@
  */
 package mp.g17.posts;
 
+import mp.g17.users.Alumno;
+import mp.g17.users.Usuario;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 /**
- *
  * @author usuario
  */
 public class PreguntaEncuestaTest {
 
+    private final Usuario userTest1;
+    private final Usuario userTest2;
+    private final Usuario userTest3;
+
     public PreguntaEncuestaTest() {
+        userTest1 = new Alumno("Hugo", "Vizcaino", "hvizcaino", "hugo@urjc.es", "1234");
+        userTest2 = new Alumno("Hugo2", "Vizcaino2", "hvizcaino2", "hugo2@urjc.es", "1234");
+        userTest3 = new Alumno("Hugo3", "Vizcaino3", "hvizcaino3", "hugo3@urjc.es", "1234");
     }
 
     /**
@@ -21,20 +34,11 @@ public class PreguntaEncuestaTest {
      */
     @Test
     public void testAddAnswer() {
-    }
+        PreguntaEncuesta preguntaEncuesta = new PreguntaEncuesta("Pregunta 1");
+        preguntaEncuesta.addAnswer("Respuesta 1");
+        preguntaEncuesta.addAnswer("Respuesta 2");
+        assertEquals(2, preguntaEncuesta.getAnswers().size());
 
-    /**
-     * Test of getQuestion method, of class PreguntaEncuesta.
-     */
-    @Test
-    public void testGetQuestion() {
-    }
-
-    /**
-     * Test of setQuestion method, of class PreguntaEncuesta.
-     */
-    @Test
-    public void testSetQuestion() {
     }
 
     /**
@@ -42,20 +46,35 @@ public class PreguntaEncuestaTest {
      */
     @Test
     public void testGetAnswersRegistered() {
+        PreguntaEncuesta preguntaEncuesta = new PreguntaEncuesta("Pregunta 1");
+        preguntaEncuesta.addAnswer("Respuesta 1");
+        preguntaEncuesta.addAnswer("Respuesta 2");
+        preguntaEncuesta.answer(userTest1, "Si");
+        preguntaEncuesta.answer(userTest2, "No");
+        preguntaEncuesta.answer(userTest3, "Niet");
+        Map<Usuario, String> answersExpected = new HashMap<>();
+        answersExpected.put(userTest1, "Si");
+        answersExpected.put(userTest2, "No");
+        assertEquals(answersExpected, preguntaEncuesta.getAnswersRegistered());
     }
 
     /**
      * Test of answer method, of class PreguntaEncuesta.
      */
     @Test
-    public void testAnswer() {
+    public void testAnswerSuccess() {
+        PreguntaEncuesta preguntaEncuesta = new PreguntaEncuesta("Pregunta 1");
+        preguntaEncuesta.addAnswer("Respuesta 1");
+        preguntaEncuesta.answer(userTest1, "Respuesta 1");
+        assertEquals(1, preguntaEncuesta.getAnswersRegistered().size());
     }
 
-    /**
-     * Test of getAnswers method, of class PreguntaEncuesta.
-     */
     @Test
-    public void testGetAnswers() {
+    public void testAnswerInvalid() {
+        PreguntaEncuesta preguntaEncuesta = new PreguntaEncuesta("Pregunta 1");
+        preguntaEncuesta.addAnswer("Respuesta 1");
+        preguntaEncuesta.answer(userTest3, "Respuesta asereje");
+        assertEquals(0, preguntaEncuesta.getAnswersRegistered().size());
     }
 
 }
