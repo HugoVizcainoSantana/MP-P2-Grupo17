9eb9bba613d115196d616d62241d242476893f82
sarry150 <sergiosarriaporro@gmail.com>
2020-05-15 20:07:54 +0200

    Test subforo fixed2

--- test/mp/g17/SubforoTest.java
+++ test/mp/g17/SubforoTest.java
@@ -5,7 +5,11 @@
  */
 package mp.g17;
 
+import java.util.List;
 import mp.g17.posts.Entrada;
+import mp.g17.posts.comparer.SortByPointsStrategy;
+import mp.g17.posts.comparer.SortType;
+import mp.g17.users.Administrador;
 import mp.g17.users.Profesor;
 import org.junit.After;
 import static org.junit.Assert.*;
@@ -78,6 +82,74 @@ public class SubforoTest {
         System.out.println("Numero de entradas en el foro: " + sub.getNumberEntries());
         System.out.println("Numero de entradas verificadas en el foro: " + sub.getNumberOfVerfiedPosts());
         assertTrue(sub.getNumberOfVerfiedPosts() == 0);
+    }
+    
+    @Test
+    public void testgetPostsVotesDescending(){
+        Profesor prof = new Profesor("Jose", "Perez", "", "j.perez@.urjc.es", "12345");
+        Administrador admin = new Administrador("a.perez@urjc.es", "12345");
+        Subforo sub = new Subforo(prof, "Profesores");
+        Entrada entry = new Entrada(prof, "Ejercicios");
+        Entrada entry1 = new Entrada(prof, "Resolucion 1");
+        Entrada entry2 = new Entrada(prof, "Resolucion 2");
+        Entrada entry3 = new Entrada(prof, "Resolucion 3");
+        entry.setPoints(12);
+        entry1.setPoints(74);
+        entry2.setPoints(63);
+        entry3.setPoints(8);
+        admin.verify(entry, true);
+        admin.verify(entry1, true);
+        admin.verify(entry2, true);
+        admin.verify(entry3, true);
+        sub.addNewEntry(entry1);
+        sub.addNewEntry(entry3);
+        sub.addNewEntry(entry2);
+        sub.addNewEntry(entry);
+        admin.updatePosts(sub);
+        sub.setSortingStrategy(new SortByPointsStrategy(SortType.DESCENDING));
+        List<Entrada> list = sub.getPosts();
+        boolean comp = false;
+        System.out.println(list.size());
+        for(int i = 0; i <= list.size() - 2; i++){
+            if(list.get(i).getPoints() > list.get(i + 1).getPoints()){
+                comp = true;
+            }
+        }
+        assertEquals(comp, true);
+    }
+    
+    @Test
+    public void testgetPostsVotesAscending(){
+        Profesor prof = new Profesor("Jose", "Perez", "", "j.perez@.urjc.es", "12345");
+        Administrador admin = new Administrador("a.perez@urjc.es", "12345");
+        Subforo sub = new Subforo(prof, "Profesores");
+        Entrada entry = new Entrada(prof, "Ejercicios");
+        Entrada entry1 = new Entrada(prof, "Resolucion 1");
+        Entrada entry2 = new Entrada(prof, "Resolucion 2");
+        Entrada entry3 = new Entrada(prof, "Resolucion 3");
+        entry.setPoints(12);
+        entry1.setPoints(74);
+        entry2.setPoints(63);
+        entry3.setPoints(8);
+        admin.verify(entry, true);
+        admin.verify(entry1, true);
+        admin.verify(entry2, true);
+        admin.verify(entry3, true);
+        sub.addNewEntry(entry1);
+        sub.addNewEntry(entry3);
+        sub.addNewEntry(entry2);
+        sub.addNewEntry(entry);
+        admin.updatePosts(sub);
+        sub.setSortingStrategy(new SortByPointsStrategy(SortType.ASCENDING));
+        List<Entrada> list = sub.getPosts();
+        boolean comp = false;
+        System.out.println(list.size());
+        for(int i = 0; i <= list.size() - 2; i++){
+            if(list.get(i).getPoints() < list.get(i + 1).getPoints()){
+                comp = true;
+            }
+        }
+        assertEquals(comp, true);
     }
 
 }
